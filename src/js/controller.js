7637465bb08cdd3d11075faa5d19a5b530313e38
Obua Denis Daniel <dd.obua@gmail.com>
2024-01-09 15:45:41 +0300

    Load recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,1 +1,26 @@
-  
+const showRecipe = async () => {
+  try {
+    // Loading recipe
+    const res = await fetch(
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604691c37cdc054bd0bc'
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${res.status} - ${data.message}`);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (error) {
+    console.error(error);
+  }
+};
+showRecipe();
