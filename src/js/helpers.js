316a9896b9afcde223147d1b2020262f64886220
Obua Denis Daniel <dd.obua@gmail.com>
2024-01-11 09:54:12 +0300

    Set AJAX request timeout

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,8 +1,22 @@
 import { API_URL } from './config.js';
+import { TIMEOUT_SECONDS } from './config.js';
+
+const timeout = (s) => {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject(
+        new Error(
+          `Request took too long! Timeout after ${s} second${s !== 1 ? 's' : ''}.`
+        )
+      );
+    }, s * 1000);
+  });
+};
 
 export const getData = async (url) => {
   try {
-    const res = await fetch(url);
+    const promise = fetch(url);
+    const res = await Promise.race([promise, timeout(TIMEOUT_SECONDS)]);
     const data = await res.json();
     if (!res.ok) throw new Error(`${res.status} - ${data.message}`);
     return data;
